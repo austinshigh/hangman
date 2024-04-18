9f739e6c09199df754828bfbe438bcfe974186dd
Austin <austinstewarthigh@gmail.com>
2024-04-18 17:40:39 -0400

    stubbing out Settings and Stats

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Homepage from "./pages/Homepage";
+import { useState } from "react";
 
 function App() {
+  const [totalWins, setTotalWins] = useState(0);
+  const [totalGuessesForWins, setTotalGuessesForWins] = useState(0);
+
+  const [lowerBound, setLowerBound] = useState(null);
+  const [upperBound, setUpperBound] = useState(null);
+  const [guessLimit, setGuessLimit] = useState(null);
+
+  // functions for Homepage
+  const incrementTotalWins = () => {
+    setTotalWins(totalWins + 1);
+  };
+
+  const addToTotalGuessesForWins = (count) => {
+    setTotalGuessesForWins(totalGuessesForWins + count);
+  };
+
+  // functions for settings page
+
+  const handleSetLowerBound = (e) => {
+    setLowerBound(e.target.value);
+  };
+
+  const handleSetUpperBound = (e) => {
+    setUpperBound(e.target.value);
+  };
+
+  const handleSetGuessLimit = (e) => {
+    setGuessLimit(e.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +41,19 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Homepage />
+        <Homepage
+          guessLimit={guessLimit}
+          lowerBound={lowerBound}
+          upperBound={upperBound}
+          addToTotalGuessesForWin={addToTotalGuessesForWins}
+          incrementTotalWins={incrementTotalWins}
+        />
+        {/* <Settings
+          handleSetLowerBound={handleSetLowerBound}
+          handleSetUpperBound={handleSetUpperBound}
+          handleSetGuessLimit={handleSetGuessLimit}
+        />
+        <Stats wins={totalWins} guesses={totalGuessesForWins} /> */}
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,27 +1,60 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 
-const Homepage = () => {
+const Homepage = (props) => {
   const [randomNumber, setRandomNumber] = useState(null);
-  const [guess, setGuess] = useState(0);
-  const [remainingGuesses, setRemainingGuesses] = useState(5);
+  const [guess, setGuess] = useState(null);
+  const [remainingGuesses, setRemainingGuesses] = useState(props.guessLimit);
+  const [totalGuesses, setTotalGuesses] = useState(0);
+  const [hint, setHint] = useState(null);
+  const [victory, setVictory] = useState(false);
+  const [loss, setLoss] = useState(false);
 
   const generateRandomNumber = () => {
-    let max = 100;
+    let max = props.upperBound;
+    let min = props.lowerBound;
+    //TODO add lowerbound to calculation
     return Math.floor(Math.random() * max);
   };
 
-  const handleClickGenerate = (e) => {
+  const handleClickNewGame = (e) => {
     e.preventDefault();
+    setGuess(null);
+    setRemainingGuesses(props.guessLimit);
+    setHint(null);
+    setVictory(false);
+    setLoss(false);
     setRandomNumber(generateRandomNumber());
   };
 
   const handleClickGuess = (e) => {
     e.preventDefault();
-    if (randomNumber === Number.parseInt(guess)) {
-      alert("u win");
-    } else {
-      setRemainingGuesses(remainingGuesses - 1);
+    setTotalGuesses(totalGuesses + 1);
+    if (!loss && !victory) {
+      const currentGuess = Number.parseInt(guess);
+      if (randomNumber > currentGuess) {
+        setHint("Too Low");
+      } else if (randomNumber < currentGuess) {
+        setHint("Too High");
+      } else if (randomNumber === currentGuess) {
+        props.incrementTotalWins();
+        props.addToTotalGuessesForWins(totalGuesses);
+        setHint(null);
+        setVictory(true);
+        setTotalGuesses(0);
+      }
+      if (remainingGuesses === 1) {
+        setLoss(true);
+        setRemainingGuesses(remainingGuesses - 1);
+      } else {
+        setRemainingGuesses(remainingGuesses - 1);
+      }
+    }
+  };
+
+  const handleEnterKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      handleClickGuess(e);
     }
   };
 
@@ -32,11 +65,17 @@ const Homepage = () => {
     <>
       {randomNumber !== null && (
         <>
-          <div>Guess the number {randomNumber}</div>
-          <div>Guess the number {remainingGuesses}</div>
-          <input onChange={(e) => setGuess(e.target.value)}></input>
+          {victory && <div>You Win!</div>}
+          {loss && <div>You Lose.</div>}
+          {victory || (loss && <div>Correct Answer: {randomNumber}</div>)}
+          <div>Remaining Guesses: {remainingGuesses}</div>
+          {hint !== null && <div>{hint}</div>}
+          <input
+            onChange={(e) => setGuess(e.target.value)}
+            onKeyDown={(e) => handleEnterKeyPress(e)}
+          ></input>
           <button onClick={(e) => handleClickGuess(e)}>Submit Guess</button>
-          <button onClick={(e) => handleClickGenerate(e)}>New Number</button>
+          <button onClick={(e) => handleClickNewGame(e)}>New Game</button>
         </>
       )}
     </>
